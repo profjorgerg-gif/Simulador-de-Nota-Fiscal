eb546760a642f58443c578a629d0531318f021e4
Projetos Pedagógicos <profjorge.rg@gmail.com>
2026-09-09 10:52:46 -0300

    Ativar versão 2.2 e estilo de impressão retrato

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppV2 from "./AppV2";
+import ReleaseBanner from "./ReleaseBanner";
 import "./index.css";
+import "./print.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <ReleaseBanner />
     <AppV2 />
   </React.StrictMode>
 );
