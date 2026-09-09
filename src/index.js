8b6315a087f55d739a8a38b3b7fb6d33f4a595ac
Projetos Pedagógicos <profjorge.rg@gmail.com>
2026-09-09 15:57:33 -0300

    Implantar Motor Fiscal 2026.1 com validações tributárias

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppV3 from "./AppV3";
+import AppV4 from "./AppV4";
 import ReleaseBanner from "./ReleaseBanner";
 import LegalFooter from "./LegalFooter";
 import LandingGate from "./LandingGate";
@@ -27,7 +27,7 @@ root.render(
   <React.StrictMode>
     <LandingGate>
       <ReleaseBanner />
-      <AppV3 />
+      <AppV4 />
       <LegalFooter />
     </LandingGate>
   </React.StrictMode>
