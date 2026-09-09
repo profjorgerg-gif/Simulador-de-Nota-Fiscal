f2bcecc26154a1a47acdb106f3980a346838140e
Projetos Pedagógicos <profjorge.rg@gmail.com>
2026-09-09 11:13:35 -0300

    Exibir direitos autorais no rodapé da plataforma

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppV3 from "./AppV3";
 import ReleaseBanner from "./ReleaseBanner";
+import LegalFooter from "./LegalFooter";
 import "./index.css";
 import "./print.css";
 import "./pdfview.css";
+import "./legalfooter.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ReleaseBanner />
     <AppV3 />
+    <LegalFooter />
   </React.StrictMode>
 );
