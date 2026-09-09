69301f019806a3416acd8489e0e8d0f02812bd45
Projetos Pedagógicos <profjorge.rg@gmail.com>
2026-09-09 09:23:41 -0300

    Corrigir configuração do Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// TODO: cole aqui o firebaseConfig copiado do Console do Firebase
-// (Configurações do projeto → Geral → Seus apps → objeto firebaseConfig)
 const firebaseConfig = {
-  apiKey: "TODO_API_KEY",
-  authDomain: "TODO_PROJETO.firebaseapp.com",
-  projectId: "TODO_PROJETO",
-  storageBucket: "TODO_PROJETO.appspot.com",
-  messagingSenderId: "TODO_SENDER_ID",
-  appId: "TODO_APP_ID",
+  apiKey: "AIzaSyDQP739dg32HEZ_RbkmaRGxgMtsS7LHpIM",
+  authDomain: "simulador-de-nota-fiscal.firebaseapp.com",
+  projectId: "simulador-de-nota-fiscal",
+  storageBucket: "simulador-de-nota-fiscal.firebasestorage.app",
+  messagingSenderId: "604957784302",
+  appId: "1:604957784302:web:3ba10390a7d792433d9bff",
 };
 
 const app = initializeApp(firebaseConfig);
