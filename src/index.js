4d7d63a95f3ffcca1480e8872610d06a2e76cabb
Projetos Pedagógicos <profjorge.rg@gmail.com>
2026-09-09 10:42:38 -0300

    Ativar versão com PDF retrato e seleção em bloco

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import AppV2 from "./AppV2";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppV2 />
   </React.StrictMode>
 );
